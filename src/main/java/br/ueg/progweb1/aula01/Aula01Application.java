a81db27b77d91bd351d319bdfd39bb2edae147fd
Guiliano Rangel <guiliano@gmail.com>
2024-04-17 20:23:53 -0300

    ADD - Configuração para definir variáveis de conexão swagger e anco de dados no docker por variável de ambiente.

--- src/main/java/br/ueg/progweb1/aula01/Aula01Application.java
+++ src/main/java/br/ueg/progweb1/aula01/Aula01Application.java
@@ -1,9 +1,12 @@
 package br.ueg.progweb1.aula01;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "${servidor.url}", description = "Default Server URL")})
 public class Aula01Application {
 
 	public static void main(String[] args) {
